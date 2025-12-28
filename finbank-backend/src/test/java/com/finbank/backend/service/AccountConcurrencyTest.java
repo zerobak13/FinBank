343bf0a35ddac5ffa4c2e59222c6f3baf70fe714
박제영 <bagjeyeong@bagjeyeong-ui-MacBookPro.local>
2025-12-28 19:10:40 +0900

    ready to test

---
+++ finbank-backend/src/test/java/com/finbank/backend/service/AccountConcurrencyTest.java
@@ -0,0 +1,13 @@
+package com.finbank.backend.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountConcurrencyTest {
+
+    @Test
+    void 테스트_환경_확인() {
+        System.out.println("테스트 실행 성공");
+    }
+}
