87c28382e36093884e1bc22ed4ba1de9cfaaee3c
zerobak13 <pjy0807111@naver.com>
2026-01-24 02:51:01 +0900

    modify transfer,deposit,withdraw

--- finbank-backend/src/main/java/com/finbank/backend/controller/AccountController.java
+++ finbank-backend/src/main/java/com/finbank/backend/controller/AccountController.java
@@ -36,6 +36,25 @@ public class AccountController {
         return ResponseEntity.ok(accountService.getAccountDetail(id));
     }
 
+    @PostMapping("/{id}/deposit")
+    public ResponseEntity<Map<String, String>> deposit(
+            @PathVariable Long id,
+            @RequestParam Long amount
+    ) {
+        accountService.deposit(id, amount);
+        return ResponseEntity.ok(Map.of("message", "입금 완료"));
+    }
+
+    @PostMapping("/{id}/withdraw")
+    public ResponseEntity<Map<String, String>> withdraw(
+            @PathVariable Long id,
+            @RequestParam Long amount
+    ) {
+        accountService.withdraw(id, amount);
+        return ResponseEntity.ok(Map.of("message", "출금 완료"));
+    }
+
+
     @PostMapping("/transfer")
     public ResponseEntity<Map<String, String>> transfer(
             @Valid @RequestBody TransferRequest request) {
